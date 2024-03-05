b540a476f99c8c7a2e2177b38f8e8eaecc35e227
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-03-06 00:12:54 +0800

    feat: update

---
+++ src/main/java/com/alibaba/yjopenapi/client/model/MultiplayLeaveResultModel.java
@@ -0,0 +1,78 @@
+/*
+ * 
+ * YuanJing OpenAPI SDK
+ *
+ */
+
+package com.alibaba.yjopenapi.client.model;
+
+import java.util.Objects;
+import java.util.Arrays;
+import java.io.Serializable;
+/**
+ * MultiplayLeaveResultModel
+ */
+public class MultiplayLeaveResultModel implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private String dataVer = null;
+
+
+  public MultiplayLeaveResultModel dataVer(String dataVer) {
+    this.dataVer = dataVer;
+    return this;
+  }
+
+   /**
+   * Get dataVer
+   * @return dataVer
+  **/
+  public String getDataVer() {
+    return dataVer;
+  }
+
+  public void setDataVer(String dataVer) {
+    this.dataVer = dataVer;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MultiplayLeaveResultModel multiplayLeaveResultModel = (MultiplayLeaveResultModel) o;
+    return Objects.equals(this.dataVer, multiplayLeaveResultModel.dataVer);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(dataVer);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class MultiplayLeaveResultModel {");
+    
+    sb.append(",dataVer: ").append(toIndentedString(dataVer));
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString();
+  }
+
+}
