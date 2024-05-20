d71b725cd01e5c2ac6cd7c53333a34f43eb088b5
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-05-20 15:00:57 +0800

    feat: update

---
+++ src/main/java/com/alibaba/yjopenapi/client/model/ConsoleAdminCreateProjectResultModel.java
@@ -0,0 +1,78 @@
+/*
+ * 
+ * YuanJing OpenAPI SDK
+ *
+ */
+
+package com.alibaba.yjopenapi.client.model;
+
+import java.util.Objects;
+import java.util.Arrays;
+import java.io.Serializable;
+/**
+ * ConsoleAdminCreateProjectResultModel
+ */
+public class ConsoleAdminCreateProjectResultModel implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private String projectId = null;
+
+
+  public ConsoleAdminCreateProjectResultModel projectId(String projectId) {
+    this.projectId = projectId;
+    return this;
+  }
+
+   /**
+   * Get projectId
+   * @return projectId
+  **/
+  public String getProjectId() {
+    return projectId;
+  }
+
+  public void setProjectId(String projectId) {
+    this.projectId = projectId;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ConsoleAdminCreateProjectResultModel consoleAdminCreateProjectResultModel = (ConsoleAdminCreateProjectResultModel) o;
+    return Objects.equals(this.projectId, consoleAdminCreateProjectResultModel.projectId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(projectId);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ConsoleAdminCreateProjectResultModel {");
+    
+    sb.append(",projectId: ").append(toIndentedString(projectId));
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString();
+  }
+
+}
