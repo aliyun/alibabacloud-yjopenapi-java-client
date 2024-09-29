73a44fd6ab6bef4ace7a8d0cada5f96abce8f795
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-09-30 00:15:11 +0800

    feat: update

---
+++ src/main/java/com/alibaba/yjopenapi/client/model/QueryGameHangForms.java
@@ -0,0 +1,100 @@
+/*
+ * 
+ * YuanJing OpenAPI SDK
+ *
+ */
+
+package com.alibaba.yjopenapi.client.model;
+
+import java.util.Objects;
+import java.util.Arrays;
+import java.io.Serializable;
+/**
+ * QueryGameHangForms
+ */
+public class QueryGameHangForms implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private String gameSession = null;
+
+
+  private String appKey = null;
+
+
+  public QueryGameHangForms gameSession(String gameSession) {
+    this.gameSession = gameSession;
+    return this;
+  }
+
+   /**
+   * 会话ID
+   * @return gameSession
+  **/
+  public String getGameSession() {
+    return gameSession;
+  }
+
+  public void setGameSession(String gameSession) {
+    this.gameSession = gameSession;
+  }
+
+  public QueryGameHangForms appKey(String appKey) {
+    this.appKey = appKey;
+    return this;
+  }
+
+   /**
+   * 项目应用AK
+   * @return appKey
+  **/
+  public String getAppKey() {
+    return appKey;
+  }
+
+  public void setAppKey(String appKey) {
+    this.appKey = appKey;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    QueryGameHangForms queryGameHangForms = (QueryGameHangForms) o;
+    return Objects.equals(this.gameSession, queryGameHangForms.gameSession) &&
+        Objects.equals(this.appKey, queryGameHangForms.appKey);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(gameSession, appKey);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class QueryGameHangForms {");
+    
+    sb.append(",gameSession: ").append(toIndentedString(gameSession));
+    sb.append(",appKey: ").append(toIndentedString(appKey));
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString();
+  }
+
+}
