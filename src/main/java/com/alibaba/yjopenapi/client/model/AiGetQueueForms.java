bd6a40aa90f8f63316f6b1884300cfa2cfa5562e
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-10-16 00:10:37 +0800

    feat: update

---
+++ src/main/java/com/alibaba/yjopenapi/client/model/AiGetQueueForms.java
@@ -0,0 +1,78 @@
+/*
+ * 
+ * YuanJing OpenAPI SDK
+ *
+ */
+
+package com.alibaba.yjopenapi.client.model;
+
+import java.util.Objects;
+import java.util.Arrays;
+import java.io.Serializable;
+/**
+ * AiGetQueueForms
+ */
+public class AiGetQueueForms implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private String taskId = null;
+
+
+  public AiGetQueueForms taskId(String taskId) {
+    this.taskId = taskId;
+    return this;
+  }
+
+   /**
+   * Get taskId
+   * @return taskId
+  **/
+  public String getTaskId() {
+    return taskId;
+  }
+
+  public void setTaskId(String taskId) {
+    this.taskId = taskId;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AiGetQueueForms aiGetQueueForms = (AiGetQueueForms) o;
+    return Objects.equals(this.taskId, aiGetQueueForms.taskId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(taskId);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class AiGetQueueForms {");
+    
+    sb.append(",taskId: ").append(toIndentedString(taskId));
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString();
+  }
+
+}
