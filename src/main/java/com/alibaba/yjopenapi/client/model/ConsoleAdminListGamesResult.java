821699b5db623b087bcdeb04c01661c33a0aa02b
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-10-24 00:13:09 +0800

    feat: update

---
+++ src/main/java/com/alibaba/yjopenapi/client/model/ConsoleAdminListGamesResult.java
@@ -0,0 +1,145 @@
+/*
+ * 
+ * YuanJing OpenAPI SDK
+ *
+ */
+
+package com.alibaba.yjopenapi.client.model;
+
+import java.util.Objects;
+import java.util.Arrays;
+import com.alibaba.yjopenapi.client.model.ConsoleAdminListGamesResultModel;
+import java.io.Serializable;
+/**
+ * ConsoleAdminListGamesResult
+ */
+public class ConsoleAdminListGamesResult implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private Boolean success = null;
+
+
+  private ConsoleAdminListGamesResultModel model = null;
+
+
+  private String msgInfo = null;
+
+
+  private String msgCode = null;
+
+
+  public ConsoleAdminListGamesResult success(Boolean success) {
+    this.success = success;
+    return this;
+  }
+
+   /**
+   * Get success
+   * @return success
+  **/
+  public Boolean getSuccess() {
+    return success;
+  }
+
+  public void setSuccess(Boolean success) {
+    this.success = success;
+  }
+
+  public ConsoleAdminListGamesResult model(ConsoleAdminListGamesResultModel model) {
+    this.model = model;
+    return this;
+  }
+
+   /**
+   * Get model
+   * @return model
+  **/
+  public ConsoleAdminListGamesResultModel getModel() {
+    return model;
+  }
+
+  public void setModel(ConsoleAdminListGamesResultModel model) {
+    this.model = model;
+  }
+
+  public ConsoleAdminListGamesResult msgInfo(String msgInfo) {
+    this.msgInfo = msgInfo;
+    return this;
+  }
+
+   /**
+   * Get msgInfo
+   * @return msgInfo
+  **/
+  public String getMsgInfo() {
+    return msgInfo;
+  }
+
+  public void setMsgInfo(String msgInfo) {
+    this.msgInfo = msgInfo;
+  }
+
+  public ConsoleAdminListGamesResult msgCode(String msgCode) {
+    this.msgCode = msgCode;
+    return this;
+  }
+
+   /**
+   * Get msgCode
+   * @return msgCode
+  **/
+  public String getMsgCode() {
+    return msgCode;
+  }
+
+  public void setMsgCode(String msgCode) {
+    this.msgCode = msgCode;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ConsoleAdminListGamesResult consoleAdminListGamesResult = (ConsoleAdminListGamesResult) o;
+    return Objects.equals(this.success, consoleAdminListGamesResult.success) &&
+        Objects.equals(this.model, consoleAdminListGamesResult.model) &&
+        Objects.equals(this.msgInfo, consoleAdminListGamesResult.msgInfo) &&
+        Objects.equals(this.msgCode, consoleAdminListGamesResult.msgCode);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(success, model, msgInfo, msgCode);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ConsoleAdminListGamesResult {");
+    
+    sb.append(",success: ").append(toIndentedString(success));
+    sb.append(",model: ").append(toIndentedString(model));
+    sb.append(",msgInfo: ").append(toIndentedString(msgInfo));
+    sb.append(",msgCode: ").append(toIndentedString(msgCode));
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString();
+  }
+
+}
